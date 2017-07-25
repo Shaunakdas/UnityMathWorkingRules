a6002f3f7687b7a3602fb3a9bad340789eb2fe19
Shaunak Das <shaunakdas2020@gmail.com>
2017-07-25 14:12:49 +0530

    Score Calculator Logic added

---
+++ Assets/Scripts/ScoreBoard/ScoreCalculator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCalculator : MonoBehaviour {
+	public const int MAX_SCORE_VALUE = 3000,MIN_SCORE_VALUE = 1000;
+	public const float MIN_ITEM_TIME = 0.33f;
+	public const float MAX_ITEM_TIME = 0.66f;
+	public float maxItemScore=0f,minItemScore=0f;
+	public enum Result{Correct,Incorrect,Seen,Timeout}
+	public void generateAttrs(int _questionCount){
+		maxItemScore = MAX_SCORE_VALUE / _questionCount;
+		minItemScore = maxItemScore / 3;
+	}
+	public float itemScore(Result _itemResult, float _timeTaken){
+		float _itemScore = 0f;
+//		float maxScore = maxItemScore,minScore = minItemScore,maxTime = MAX_ITEM_TIME,minTime = MIN_ITEM_TIME;
+		switch (_itemResult) {
+		case Result.Correct:
+			_itemScore = correctScoreFormula( _timeTaken, MIN_ITEM_TIME, MAX_ITEM_TIME, minItemScore, maxItemScore);
+			break;
+		case Result.Incorrect:
+			_itemScore = 0f;
+			break;
+		case Result.Timeout:
+			_itemScore = 0f;
+			break;
+		}
+		return _itemScore;
+	}
+	public float correctScoreFormula(float _timeTaken, float _minTime, float _maxTime, float _minScore, float _maxScore){
+		return _maxScore - ((_timeTaken - _minTime) * (_maxScore - _minScore) / (_maxTime - _minTime));
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
